fe1eab6c0a85a06b2dbd3c0aa1cec5364d77fb75
21godman <michaelkuo0617@gmail.com>
2024-04-06 18:36:15 +0800

    add read file

--- products.py
+++ products.py
@@ -1,4 +1,19 @@
+# 讀取檔案
 products = []
+
+with open('products.csv', 'r', encoding = 'utf-8') as f:
+	for line in f:
+		# 跳過 header
+		if '商品,價格' in line: 
+			continue	# 跳過此次迴圈，繼續執行下一個迴圈(break是跳出迴圈)
+		# 先除掉換行符號(\n)，再用逗點做分割, 分別儲存在 name 和 price
+		# split 完會變清單 list
+		name, price = line.strip().split(',')	
+		products.append([name, price])
+
+print(products)
+
+# 輸入新的清單內容
 while True:
 	name = input('請輸入商品名稱: ')
 	if name == 'q':
@@ -13,7 +28,7 @@ while True:
 	# 簡易寫法
 	p = [name, price]
 
-	products.append(p) # 也可以不定義 p 直接把 list 寫到括號裡面
+	products.append(p)	# 也可以不定義 p 直接把 list 寫到括號裡面
 print(products)
 
 # 存取二維清單
