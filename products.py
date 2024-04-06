07f3c5316ca461dfec78d93d4578923b07675fd4
21godman <michaelkuo0617@gmail.com>
2024-04-06 19:41:09 +0800

    add check file

--- products.py
+++ products.py
@@ -1,17 +1,22 @@
-# 讀取檔案
+# 檢查檔案是否存在
+import os # operating system
+
 products = []
-
-with open('products.csv', 'r', encoding = 'utf-8') as f:
-	for line in f:
-		# 跳過 header
-		if '商品,價格' in line: 
-			continue	# 跳過此次迴圈，繼續執行下一個迴圈(break是跳出迴圈)
-		# 先除掉換行符號(\n)，再用逗點做分割, 分別儲存在 name 和 price
-		# split 完會變清單 list
-		name, price = line.strip().split(',')	
-		products.append([name, price])
-
-print(products)
+if os.path.isfile('products.csv'):
+	print('YA! 找到檔案了!')
+	# 讀取檔案
+	with open('products.csv', 'r', encoding = 'utf-8') as f:
+		for line in f:
+			# 跳過 header
+			if '商品,價格' in line: 
+				continue	# 跳過此次迴圈，繼續執行下一個迴圈(break是跳出迴圈)
+			# 先除掉換行符號(\n)，再用逗點做分割, 分別儲存在 name 和 price
+			# split 完會變清單 list
+			name, price = line.strip().split(',')	
+			products.append([name, price])
+	print(products)
+else:
+	print('找不到檔案...')
 
 # 輸入新的清單內容
 while True:
@@ -26,9 +31,11 @@ while True:
 	# p.append(price)
 
 	# 簡易寫法
-	p = [name, price]
+	# p = [name, price]
+	# products.append(p)
 
-	products.append(p)	# 也可以不定義 p 直接把 list 寫到括號裡面
+	#更簡易寫法
+	products.append([name, price])
 print(products)
 
 # 存取二維清單
@@ -38,8 +45,8 @@ print(products)
 for p in products:
 	print(p[0], '的價格是', p[1])
 
+# 寫入檔案
 # str 可以做 + 和 *，int 不可以
-# 寫入一個csv檔
 # 解決程式編碼問題(encoding)
 with open('products.csv', 'w', encoding = 'utf-8') as f:
 	# 加入欄位
